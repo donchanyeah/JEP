bd4427000ceb9e4cfd6fa06658107f45aa0c8512
donchanyeah <51076418+donchanyeah@users.noreply.github.com>
2019-10-04 14:28:56 +0900

    Update install_opencv4.0.0_Nano.sh

--- script/install_opencv4.0.0_Nano.sh
+++ script/install_opencv4.0.0_Nano.sh
@@ -30,20 +30,20 @@ sudo apt-get install -y libv4l-dev v4l-utils qv4l2 v4l2ucp
 sudo apt-get install -y curl
 sudo apt-get update
 
-echo "** Download opencv-4.0.0"
+echo "** Download opencv-4.1.1"
 cd $folder
-curl -L https://github.com/opencv/opencv/archive/4.0.0.zip -o opencv-4.0.0.zip
-curl -L https://github.com/opencv/opencv_contrib/archive/4.0.0.zip -o opencv_contrib-4.0.0.zip
-unzip opencv-4.0.0.zip 
-unzip opencv_contrib-4.0.0.zip 
-cd opencv-4.0.0/
+curl -L https://github.com/opencv/opencv/archive/4.1.1.zip -o opencv-4.1.1.zip
+curl -L https://github.com/opencv/opencv_contrib/archive/4.1.1.zip -o opencv_contrib-4.1.1.zip
+unzip opencv-4.1.1.zip 
+unzip opencv_contrib-4.1.1.zip 
+cd opencv-4.1.1/
 
 echo "** Building..."
 mkdir release
 cd release/
-cmake -D WITH_CUDA=ON -D CUDA_ARCH_BIN="5.3" -D CUDA_ARCH_PTX="" -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-4.0.0/modules -D WITH_GSTREAMER=ON -D WITH_LIBV4L=ON -D BUILD_opencv_python2=ON -D BUILD_opencv_python3=ON -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_EXAMPLES=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
+cmake -D WITH_CUDA=ON -D CUDA_ARCH_BIN="5.3" -D CUDA_ARCH_PTX="" -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-4.1.1/modules -D WITH_GSTREAMER=ON -D WITH_LIBV4L=ON -D BUILD_opencv_python2=ON -D BUILD_opencv_python3=ON -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D BUILD_EXAMPLES=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
 make -j3
 sudo make install
 
-echo "** Install opencv-4.0.0 successfully"
+echo "** Install opencv-4.1.1 successfully"
 echo "** Bye :)"
